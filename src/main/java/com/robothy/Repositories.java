346e0448f21177ea181f8d9e4a8e695ff8b2af21
C5299616 <fuxiang.luo@sap.com>
2021-01-28 15:47:32 +0800

    more mirrors

--- src/main/java/com/robothy/Repositories.java
+++ src/main/java/com/robothy/Repositories.java
@@ -6,7 +6,8 @@ public class Repositories {
             "https://maven.aliyun.com/repository/public",
             "https://maven.aliyun.com/repository/jcenter",
             "https://maven.aliyun.com/repository/google",
-            "https://maven.aliyun.com/repository/gradle-plugin"
+            "https://maven.aliyun.com/repository/gradle-plugin",
+            "https://maven.aliyun.com/repository/spring"
     };
 
 }
